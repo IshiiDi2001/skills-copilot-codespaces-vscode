dcbbd9083d72bcbf924fb6b710ad009e4e8cd1c6
Ishini Dimani <129255824+IshiiDi2001@users.noreply.github.com>
2024-05-28 06:54:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['JavaScript', 'React', 'Angular', 'Vue']
+    };
+
+    console.log(member.name);
+    console.log(member.age);
+    console.log(member.skills);
+}
